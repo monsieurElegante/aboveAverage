06d0ed371f88b68292a62ab66fc40252c02e3bbd
Monsieur Elegante <monsieurelegante@icloud.com>
2020-01-27 20:04:09 -0700

    notes, removed some logger methods

--- server/main.js
+++ server/main.js
@@ -8,28 +8,28 @@ var bot = new Discord.Client({
 	autorun: true
 });
 
-bot.on('ready', function (evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
-});
+// FIXME not sure why methods isn't being loaded
+// bot.on('ready', function (evt) {
+// 	logger.info('Connected');
+// 	logger.info('Logged in as: ');
+// 	logger.info(bot.username + ' - (' + bot.id + ')');
+// });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-// Our bot needs to know if it will execute a command
-// It will listen for messages that will start with `!`
+	// It will listen for messages that will start with `!`
 	if (message.substring(0, 1) == '!') {
 		var args = message.substring(1).split(' ');
 		var cmd = args[0];
 		args = args.splice(1);
-	switch(cmd) {
-		// !ping
-		case 'ping':
+
+		// NOTE this is where commands are defined
+		switch(cmd) {
+			case 'hello':
 			bot.sendMessage({
-			to: channelID,
-			message: 'Greetings, Human! Welcome to TheDigitalWise server.'
+				to: channelID,
+				message: 'Greetings, kinkster!'
 			});
-		break;
-	// Just add any case commands if you want to..
-	}
+			break;
 		}
+	}
 });
